4ffc566959da40495d283fd2a7bc4c4bade8e945
isub897 <ivsubritzky@gmail.com>
2022-08-08 11:00:07 +1200

    navbar design without functionality

---
+++ src/components/News/News.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './News.css'
+import Headline from "./components/Headline/Headline";
+
+function News() {
+    return (
+        <div className="news">
+            <Headline />
+        </div>
+    )
+}
+
+export default News;
